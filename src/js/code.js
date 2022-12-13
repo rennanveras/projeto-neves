1b10512498bba6b677e684e4a654df7a0b5a6fd9
valeria ramalho <rennanhenriqueneves@gmail.com>
2022-12-13 00:11:44 -0300

    fiz as alterações nos nomes do cabeçalho,coloquei o texto na decricao do hero, arrumei a area de qualificações com todas as informacoes,coloquei hover nos cards e adicionei um botao para direcionar para o curriculo, diminui o tamanho dos videos da area de podcasts

--- src/js/code.js
+++ src/js/code.js
@@ -1,6 +1,6 @@
-const header = document.querySelector(".conatiner");
+/* const header = document.querySelector(".conatiner");
 header.addEventListener('scroll',diminuirHeader)
 
 function diminuirHeader(){
     
-}
+} */
