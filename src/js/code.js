d17810bfb4c3f9df0656a7ccf848810667250180
valeria ramalho <rennanhenriqueneves@gmail.com>
2022-12-24 15:28:08 -0300

    fazendo a parte respondiva de tablet

--- src/js/code.js
+++ src/js/code.js
@@ -1,18 +1,29 @@
-let buttonNavBar1 = document.querySelector('.link-navbar1');
+/* let buttonNavBar1 = document.querySelector('.link-navbar1');
 let buttonNavBar2 = document.querySelector('.link-navbar2');
 let buttonNavBar3 = document.querySelector('.link-navbar3');
 let buttonNavBar4 = document.querySelector('.link-navbar4');
 let buttonNavBar5 = document.querySelector('.link-navbar5');
-let buttonNavBar6 = document.querySelector('.link-navbar6');
+let buttonNavBar6 = document.querySelector('.link-navbar6'); */
+let buttonTogle =  document.querySelector('.nav-bar .menu-mobile');
 
 
 buttonNavBar1.addEventListener('click',(item)=>{
     item.preventDefault();
-    window.scrollTo({
-       top:100,
-       left:0,
-       behavior: "smooth"
-   }) ;
+    if(document.querySelector('.area-body"').style.width == '120rem'){
+            
+            window.scrollTo({
+            top:400,
+            left:0,
+            behavior: "smooth"
+        }) ;
+    }else{
+            item.preventDefault();
+            window.scrollTo({
+            top:800,
+            left:0,
+            behavior: "smooth"
+        }) ;
+    };
 });
 buttonNavBar2.addEventListener('click',(item)=>{
      item.preventDefault();
@@ -55,7 +66,14 @@ buttonNavBar6.addEventListener('click',(item)=>{
        behavior: "smooth"
    }) ;
 });
-
+function openMenu(){
+    if(document.querySelector('.nav-bar .ul-menu').style.display === 'none'){
+        document.querySelector('.nav-bar .ul-menu').style.display = 'block';
+    } else {
+        document.querySelector('.nav-bar .ul-menu').style.display = 'none';
+    }
+}
+buttonTogle.addEventListener('click',openMenu);
 
 function upScreen() {
     if(window.scrollY >= 500){
