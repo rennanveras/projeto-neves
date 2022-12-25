75e17006d9bd07aca34c93664a7c9825f4b026b4
rennan veras <rennanhenriqueneves@gmail.com>
2022-12-25 18:11:38 -0300

    terminei o responsivo para todos os dispositivos e a barra de navegacao mobile está 100% funcional

--- src/js/code.js
+++ src/js/code.js
@@ -1,82 +1,24 @@
-/* let buttonNavBar1 = document.querySelector('.link-navbar1');
-let buttonNavBar2 = document.querySelector('.link-navbar2');
-let buttonNavBar3 = document.querySelector('.link-navbar3');
-let buttonNavBar4 = document.querySelector('.link-navbar4');
-let buttonNavBar5 = document.querySelector('.link-navbar5');
-let buttonNavBar6 = document.querySelector('.link-navbar6'); */
-let buttonTogle =  document.querySelector('.nav-bar .menu-mobile');
+let buttonMobile =  document.querySelector('.nav-bar .menu-mobile');
+let itemMenuMobile = document.querySelectorAll('.a');
 
 
-buttonNavBar1.addEventListener('click',(item)=>{
-    item.preventDefault();
-    if(document.querySelector('.area-body"').style.width == '120rem'){
-            
-            window.scrollTo({
-            top:400,
-            left:0,
-            behavior: "smooth"
-        }) ;
-    }else{
-            item.preventDefault();
-            window.scrollTo({
-            top:800,
-            left:0,
-            behavior: "smooth"
-        }) ;
-    };
-});
-buttonNavBar2.addEventListener('click',(item)=>{
-     item.preventDefault();
-     window.scrollTo({
-        top:400,
-        left:0,
-        behavior: "smooth"
-    }) ;
-});
-buttonNavBar3.addEventListener('click',(item)=>{
-    item.preventDefault();
-    window.scrollTo({
-       top:900,
-       left:0,
-       behavior: "smooth"
-   }) ;
-});
-
-buttonNavBar4.addEventListener('click',(item)=>{
-    item.preventDefault();
-    window.scrollTo({
-       top:1762,
-       left:0,
-       behavior: "smooth"
-   }) ;
-});
-buttonNavBar5.addEventListener('click',(item)=>{
-    item.preventDefault();
-    window.scrollTo({
-       top:2636,
-       left:0,
-       behavior: "smooth"
-   }) ;
-});
-buttonNavBar6.addEventListener('click',(item)=>{
-    item.preventDefault();
-    window.scrollTo({
-       top:3454,
-       left:0,
-       behavior: "smooth"
-   }) ;
-});
-function openMenu(){
-    if(document.querySelector('.nav-bar .ul-menu').style.display === 'none'){
-        document.querySelector('.nav-bar .ul-menu').style.display = 'block';
-    } else {
-        document.querySelector('.nav-bar .ul-menu').style.display = 'none';
-    }
+function openMenu(e){
+    if(e.type === 'touchstart') e.preventDefault();
+    const nav = document.querySelector('.nav-bar');
+    nav.classList.toggle('active');
 }
-buttonTogle.addEventListener('click',openMenu);
+buttonMobile.addEventListener('click',openMenu);
+buttonMobile.addEventListener('touchstart',openMenu);
+
+itemMenuMobile.forEach((e)=>{
+    e.addEventListener('click',()=>{
+        const nav = document.querySelector('.nav-bar');
+        nav.classList.toggle('active');
+    });
+})
 
 function upScreen() {
-    if(window.scrollY >= 500){
+    if(window.scrollY >= 400){
         document.querySelector('.button-return-top').style.display = 'block';
         document.querySelector('.button-return-top').addEventListener('click', (item)=>{
             window.scrollTo({
